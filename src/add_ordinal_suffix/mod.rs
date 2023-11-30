5413ad0e975db78a88b8c574592b381fa19d4984
Pouriya Jahanbakhsh <pouriya.jahanbakhsh@gmail.com>
2023-11-30 19:43:49 +0330

    doc: update add_ordinal_suffix

--- src/add_ordinal_suffix/mod.rs
+++ src/add_ordinal_suffix/mod.rs
@@ -1,9 +1,38 @@
+//! Add persian ordinal suffix to numbers.
+//!
+//! ### Example
+//! ```rust
+//! // Use functions
+//! use rust_persian_tools::add_ordinal_suffix::{add_ordinal_suffix, add_ordinal_suffix_mut};
+//! // Or use methods (on `str`, `String`, and `Cow<'_, str>`):
+//! use rust_persian_tools::add_ordinal_suffix::{AddOrdinalSuffix, AddOrdinalSuffixMut};
+//!
+//! let input = "بیست و یک";
+//! assert_eq!(add_ordinal_suffix(input), "بیست و یکم");
+//! let mut input = input.to_string();
+//! add_ordinal_suffix_mut(&mut input);
+//! assert_eq!(input, "بیست و یکم");
+//!
+//! let input = "چهل و سه";
+//! assert_eq!(input.add_persian_ordinal_suffix(), "چهل و سوم");
+//! let mut input = input.to_string();
+//! input.add_persian_ordinal_suffix_mut();
+//! assert_eq!(input, "چهل و سوم");
+//! ```
+
 use std::borrow::Cow;
 
-///  Add Ordinal suffix to numbers <br>
-///  Example: <br>
-///  --- input: چهل و سه <br>
-///  --- output: چهل و سوم
+/// Add persian ordinal suffix to numbers.
+///
+/// Enable via `add-ordinal-suffix` Cargo feature.
+///
+/// ### Example
+/// ```rust
+/// use rust_persian_tools::add_ordinal_suffix::add_ordinal_suffix;
+///
+/// let input = "بیست و یک".to_string();
+/// assert_eq!(add_ordinal_suffix(input), "بیست و یکم");
+/// ```
 pub fn add_ordinal_suffix(number: impl AsRef<str>) -> String {
     let number = number.as_ref();
     if number.ends_with('ی') {
@@ -15,6 +44,18 @@ pub fn add_ordinal_suffix(number: impl AsRef<str>) -> String {
     }
 }
 
+/// Add persian ordinal suffix to numbers in-place.
+///
+/// Enable via `add-ordinal-suffix` Cargo feature.
+///
+/// ### Example
+/// ```rust
+/// use rust_persian_tools::add_ordinal_suffix::add_ordinal_suffix_mut;
+///
+/// let mut input = "سی".to_string();
+/// add_ordinal_suffix_mut(&mut input);
+/// assert_eq!(input, "سی اُم");
+/// ```
 pub fn add_ordinal_suffix_mut(number: &mut String) {
     if number.ends_with('ی') {
         number.push_str(" اُم")
