6308f64bb834c9be0b3fc5d53bf2b88832d6f33d
Pouriya Jahanbakhsh <pouriya.jahanbakhsh@gmail.com>
2023-11-30 15:16:38 +0330

    feat: cargo feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,29 @@
+#[cfg(feature = "add-ordinal-suffix")]
 pub mod add_ordinal_suffix;
+
+#[cfg(feature = "commas")]
 pub mod commas;
+
+#[cfg(feature = "digits")]
 pub mod digits;
+
+#[cfg(feature = "find-capital-by-province")]
 pub mod find_capital_by_province;
+
+#[cfg(feature = "is-persian")]
 pub mod is_persian;
+
+#[cfg(feature = "national-id")]
 pub mod national_id;
+
+#[cfg(feature = "remove-ordinal-suffix")]
 pub mod remove_ordinal_suffix;
+
+#[cfg(feature = "to-persian-chars")]
 pub mod to_persian_chars;
+
+#[cfg(feature = "url-fix")]
 pub mod url_fix;
+
+#[cfg(feature = "verity-card-number")]
 pub mod verity_card_number;
