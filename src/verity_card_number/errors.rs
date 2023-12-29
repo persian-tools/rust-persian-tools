48e5cd999e5a7234bfa724e4c1ab42d956282ec9
Ali Ghahremani <alighahremani1377@gmail.com>
2023-12-29 21:47:23 +0330

    improve!: refactor verity_card_number BREAKING CHANGE: verity_card_number() return type changed from bool to Result

---
+++ src/verity_card_number/errors.rs
@@ -0,0 +1,21 @@
+use std::num::ParseIntError;
+
+use thiserror::Error;
+
+#[derive(Error, Debug, PartialEq, Eq)]
+pub enum VerifyCardNumberError {
+    #[error("invalid digit")]
+    InvalidDigit,
+
+    #[error("invalid length")]
+    InvalidLength,
+
+    #[error("invalid length")]
+    InvalidCardNumber,
+}
+
+impl From<ParseIntError> for VerifyCardNumberError {
+    fn from(_: ParseIntError) -> Self {
+        VerifyCardNumberError::InvalidDigit
+    }
+}
--- src/verity_card_number/mod.rs
+++ src/verity_card_number/mod.rs
@@ -1,28 +1,30 @@
+pub mod errors;
+use errors::VerifyCardNumberError;
+
 ///  bank card number validation
-pub fn verify_card_number<S>(digits: S) -> bool
-where
-    S: Into<String>,
-{
-    let digits: String = digits.into();
+/// Example:
+/// ```
+/// # use rust_persian_tools::verity_card_number::verify_card_number;
+/// # use rust_persian_tools::verity_card_number::errors::VerifyCardNumberError;
+/// assert_eq!(verify_card_number("6037701689095443"), Ok(()));
+/// assert_eq!(
+///    verify_card_number("621986103452900"),
+///    Err(VerifyCardNumberError::InvalidLength)
+/// );
+/// ```
+/// safty: sum max posible value is 144 which is smaller than u8:MAX = 2^8 = 255
+pub fn verify_card_number(digits: impl AsRef<str>) -> Result<(), VerifyCardNumberError> {
+    let digits = digits.as_ref();
 
-    if digits.parse::<u64>().is_err() {
-        return false;
-    }
-    if digits.len() < 16 {
-        return false;
-    } // TODO why not digits.len() != 16
-    if digits[1..10].parse::<i32>().unwrap() == 0 {
-        return false;
-    }
-    if digits[10..16].parse::<i32>().unwrap() == 0 {
-        return false;
+    if digits.len() != 16 {
+        return Err(VerifyCardNumberError::InvalidLength);
     }
 
-    let mut sum = 0;
+    let mut sum = 0u8;
     for (i, digit) in digits.chars().enumerate() {
         let radix = if i % 2 == 0 { 2 } else { 1 };
 
-        let sub_digit = (digit).to_string().parse::<i32>().unwrap() * radix;
+        let sub_digit = (digit).to_string().parse::<u8>()? * radix;
         sum += if sub_digit > 9 {
             sub_digit - 9
         } else {
@@ -30,7 +32,12 @@ where
         };
     }
 
-    sum % 10 == 0
+    dbg!(&sum);
+    if sum % 10 == 0 {
+        Ok(())
+    } else {
+        Err(VerifyCardNumberError::InvalidCardNumber)
+    }
 }
 
 #[cfg(test)]
@@ -38,11 +45,32 @@ mod add_ordinal_suffix_tests {
     use super::*;
 
     #[test]
-    fn bank_number_validation() {
-        assert_eq!(verify_card_number("6037701689095443"), true);
-        assert_eq!(verify_card_number("6219861034529007"), true);
-        assert_eq!(verify_card_number("6219861034529008"), false);
-        assert_eq!(verify_card_number("621986103452900"), false);
-        assert_eq!(verify_card_number("0"), false);
+    fn card_number_validation() {
+        assert_eq!(verify_card_number("6037701689095443"), Ok(()));
+        assert_eq!(verify_card_number("6219861034529007"), Ok(()));
+    }
+
+    #[test]
+    fn card_number_validation_falsy() {
+        assert_eq!(
+            verify_card_number("62198610345290078"), // len: 17
+            Err(VerifyCardNumberError::InvalidLength)
+        );
+        assert_eq!(
+            verify_card_number("621986103452900"), // len: 15
+            Err(VerifyCardNumberError::InvalidLength)
+        );
+        assert_eq!(
+            verify_card_number("0"), // len: 1
+            Err(VerifyCardNumberError::InvalidLength)
+        );
+        assert_eq!(
+            verify_card_number("6219861A34529008"), // A
+            Err(VerifyCardNumberError::InvalidDigit)
+        );
+        assert_eq!(
+            verify_card_number("6219861034529008"), // sumcheck
+            Err(VerifyCardNumberError::InvalidCardNumber)
+        );
     }
 }
