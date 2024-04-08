1a96fd0c1a0913a6f2b4fe4ea9d7df9e60d4dc8d
Ali ghahremani <alighahremani1377@gmail.com>
2024-04-08 08:21:01 +0330

    feat: adding rest of features to arg parser and main match case

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -9,18 +9,55 @@ fn main() {
     let args = Args::parse();
 
     match args.function {
-        Function::AddCommas => {
-            handle(&args, wrapper::add_commas);
+        Function::AddCommas => handle(&args, wrapper::add_commas),
+        Function::RemoveCommas => handle(&args, wrapper::remove_commas),
+        Function::AddOrdinalSuffix => handle(&args, wrapper::add_ordinal_suffix),
+        Function::RemoveOrdinalSuffix => handle(&args, wrapper::remove_ordinal_suffix),
+        Function::HasArabic => handle(&args, wrapper::has_arabic),
+        Function::IsArabic => handle(&args, wrapper::is_arabic),
+        Function::ToArabic => handle(&args, wrapper::to_arabic),
+        Function::GetBillType => handle(&args, wrapper::get_bill_type),
+        Function::FaToEn => handle(&args, wrapper::fa_to_en),
+        Function::EnToFa => handle(&args, wrapper::en_to_fa),
+        Function::EnToAr => handle(&args, wrapper::en_to_ar),
+        Function::ArToEn => handle(&args, wrapper::ar_to_en),
+        Function::FaToAr => handle(&args, wrapper::fa_to_ar),
+        Function::ArToFa => handle(&args, wrapper::ar_to_fa),
+        Function::ExtractCardNumber => handle(&args, wrapper::extract_card_number),
+        Function::FindCapitalByProvince => handle(&args, wrapper::find_capital_by_province),
+        Function::GetBankNameByCardNumber => handle(&args, wrapper::get_bank_name_by_card_number),
+        Function::GetCityByIranNationalId => handle(&args, wrapper::get_city_by_iran_national_id),
+        Function::GetProvinceByIranNationalId => {
+            handle(&args, wrapper::get_province_by_iran_national_id)
         }
-        Function::RemoveCommas => {
-            handle(&args, wrapper::remove_commas);
-        }
+        Function::RemoveHalfSpace => handle(&args, wrapper::remove_half_space),
+        Function::AddHalfSpace => handle(&args, wrapper::add_half_space),
+        Function::VerifyIranianLegalId => handle(&args, wrapper::verify_iranian_legal_id),
+        Function::VerifyIranianNationalId => handle(&args, wrapper::verify_iranian_national_id),
+        Function::GetPlateType => handle(&args, wrapper::get_plate_type),
+        Function::GetPlateProvince => handle(&args, wrapper::get_plate_province),
+        Function::GetPlateCategory => handle(&args, wrapper::get_plate_category),
+        Function::NumberToWords => handle(&args, wrapper::number_to_words),
+        Function::HasPersian => handle(&args, wrapper::has_persian),
+        Function::IsPersian => handle(&args, wrapper::is_persian),
+        Function::ToPersianChars => handle(&args, wrapper::to_persian_chars),
+        Function::IsPhoneValid => handle(&args, wrapper::is_phone_valid),
+        Function::GetOperatorPrefix => handle(&args, wrapper::get_operator_prefix),
+        Function::GetPhoneOperator => handle(&args, wrapper::get_phone_operator),
+        Function::GetPhoneProvince => handle(&args, wrapper::get_phone_province),
+        Function::IsShebaValid => handle(&args, wrapper::is_sheba_valid),
+        Function::ShebaToBankName => handle(&args, wrapper::sheba_to_bank_name),
+        Function::ShebaToPersianBankName => handle(&args, wrapper::sheba_to_persian_bank_name),
+        Function::TimeDiff => handle(&args, wrapper::time_diff),
+        Function::UrlFix => handle(&args, wrapper::url_fix),
+        Function::VerifyCardNumber => handle(&args, wrapper::verify_card_number),
+        Function::WordsToNumber => handle(&args, wrapper::words_to_number),
     }
 }
 
-fn handle(args: &Args, exe: fn(&str, &Args) -> String) {
+fn handle(args: &Args, exe: fn(&str) -> String) {
     if let Some(x) = &args.input {
-        println!("{}", exe(x, &args));
+        println!("{}", exe(x));
     } else {
         let mut buffer = String::new();
         loop {
@@ -32,7 +69,7 @@ fn handle(args: &Args, exe: fn(&str, &Args) -> String) {
             if buffer.is_empty() {
                 break;
             }
-            println!("{}", exe(&buffer, &args));
+            println!("{}", exe(&buffer));
         }
     }
 }
--- cli/src/wrapper.rs
+++ cli/src/wrapper.rs
@@ -1,36 +1,35 @@
 use std::str::FromStr;
 
 use crate::constants::*;
-use crate::Args;
 use rust_persian_tools::*;
 
-pub fn add_commas(s: &str, _: &Args) -> String {
+pub fn add_commas(s: &str) -> String {
     commas::add_commas::add_commas(s)
 }
-pub fn remove_commas(s: &str, _: &Args) -> String {
+pub fn remove_commas(s: &str) -> String {
     commas::remove_commas::remove_commas(s)
 }
 // ---
-pub fn add_ordinal_suffix(s: &str, _: &Args) -> String {
+pub fn add_ordinal_suffix(s: &str) -> String {
     add_ordinal_suffix::add_ordinal_suffix(s)
 }
 // ---
-pub fn remove_ordinal_suffix(s: &str, _: &Args) -> String {
+pub fn remove_ordinal_suffix(s: &str) -> String {
     remove_ordinal_suffix::remove_ordinal_suffix(s)
 }
 // --- returns true|false
-pub fn has_arabic(s: &str, _: &Args) -> String {
+pub fn has_arabic(s: &str) -> String {
     arabic_chars::has_arabic(s).to_string()
 }
 // --- returns true|false
-pub fn is_arabic(s: &str, _: &Args) -> String {
+pub fn is_arabic(s: &str) -> String {
     arabic_chars::is_arabic(s).to_string()
 }
-pub fn to_arabic(s: &str, _: &Args) -> String {
+pub fn to_arabic(s: &str) -> String {
     arabic_chars::to_arabic_chars(s)
 }
 // --- invalid | <bill_type>
-pub fn get_bill_type(s: &str, args: &Args) -> String {
+pub fn get_bill_type(s: &str) -> String {
     match bill::Bill::from_str(s) {
         Ok(x) => format!("{:?}", x.get_bill_type()), // make a way to different error from ok
         Err(_) => INVALID.to_string(),
@@ -38,26 +37,26 @@ pub fn get_bill_type(s: &str, args: &Args) -> String {
 }
 
 // ---
-pub fn fa_to_en(s: &str, args: &Args) -> String {
+pub fn fa_to_en(s: &str) -> String {
     digits::fa_to_en(s)
 }
-pub fn en_to_fa(s: &str, args: &Args) -> String {
+pub fn en_to_fa(s: &str) -> String {
     digits::en_to_fa(s)
 }
-pub fn en_to_ar(s: &str, args: &Args) -> String {
+pub fn en_to_ar(s: &str) -> String {
     digits::en_to_ar(s)
 }
-pub fn ar_to_en(s: &str, args: &Args) -> String {
+pub fn ar_to_en(s: &str) -> String {
     digits::ar_to_en(s)
 }
-pub fn fa_to_ar(s: &str, args: &Args) -> String {
+pub fn fa_to_ar(s: &str) -> String {
     digits::fa_to_ar(s)
 }
-pub fn ar_to_fa(s: &str, args: &Args) -> String {
+pub fn ar_to_fa(s: &str) -> String {
     digits::ar_to_fa(s)
 }
 // --- card numbers separated with commas
-pub fn extract_card_number(s: &str, args: &Args) -> String {
+pub fn extract_card_number(s: &str) -> String {
     extract_card_number::extract_card_number(s)
         .iter()
         .map(|x| x.get_base())
@@ -65,13 +64,13 @@ pub fn extract_card_number(s: &str, args: &Args) -> String {
         .join(SEPARATOR)
 }
 // --- not-found | <capital>
-pub fn find_capital_by_province(s: &str, args: &Args) -> String {
+pub fn find_capital_by_province(s: &str) -> String {
     find_capital_by_province::find_capital_by_province(s)
         .unwrap_or(NOT_FOUND)
         .to_string()
 }
 // --- not-found
-pub fn get_bank_name_by_card_number(s: &str, args: &Args) -> String {
+pub fn get_bank_name_by_card_number(s: &str) -> String {
     match get_bank_name_by_card_number::get_bank_name_by_card_number(s) {
         Ok(x) => x.to_string(),
         Err(e) => match e {
@@ -85,13 +84,13 @@ pub fn get_bank_name_by_card_number(s: &str, args: &Args) -> String {
     }
 }
 // --- get_place_by_iran_national_id is separated to two functions
-pub fn get_city_by_iran_national_id(s: &str, args: &Args) -> String {
+pub fn get_city_by_iran_national_id(s: &str) -> String {
     match get_place_by_iran_national_id::get_place_by_iran_national_id(s) {
         Ok(x) => x.get_city().to_string(),
         Err(_) => ERROR.to_string(),
     }
 }
-pub fn get_province_by_iran_national_id(s: &str, args: &Args) -> String {
+pub fn get_province_by_iran_national_id(s: &str) -> String {
     match get_place_by_iran_national_id::get_place_by_iran_national_id(s) {
         Ok(x) => x.get_province().to_string(),
         Err(_) => ERROR.to_string(),
@@ -99,39 +98,39 @@ pub fn get_province_by_iran_national_id(s: &str, args: &Args) -> String {
 }
 
 // ---
-pub fn remove_half_space(s: &str, args: &Args) -> String {
+pub fn remove_half_space(s: &str) -> String {
     half_space::remove_half_space(s)
 }
-pub fn add_half_space(s: &str, args: &Args) -> String {
+pub fn add_half_space(s: &str) -> String {
     half_space::add_half_space(s)
 }
 
 // ---
-pub fn verify_iranian_legal_id(s: &str, args: &Args) -> String {
+pub fn verify_iranian_legal_id(s: &str) -> String {
     legal_id::verify_iranian_legal_id(s).is_ok().to_string()
 }
 // ---
-pub fn verify_iranian_national_id(s: &str, args: &Args) -> String {
+pub fn verify_iranian_national_id(s: &str) -> String {
     national_id::verify_iranian_national_id(s)
         .is_ok()
         .to_string()
 }
 // ---
-pub fn get_plate_type(s: &str, args: &Args) -> String {
+pub fn get_plate_type(s: &str) -> String {
     match rust_persian_tools::number_plate::get_plate_info(s) {
         Ok(plate) => {
             format!("{:?}", plate.plate_type)
         }
-        Err(e) => ERROR.to_string(),
+        Err(_e) => ERROR.to_string(),
     }
 }
-pub fn get_plate_province(s: &str, args: &Args) -> String {
+pub fn get_plate_province(s: &str) -> String {
     match rust_persian_tools::number_plate::get_plate_info(s) {
         Ok(plate) => plate.province,
-        Err(e) => ERROR.to_string(),
+        Err(_e) => ERROR.to_string(),
     }
 }
-pub fn get_plate_category(s: &str, args: &Args) -> String {
+pub fn get_plate_category(s: &str) -> String {
     match rust_persian_tools::number_plate::get_plate_info(s) {
         Ok(plate) => {
             if let Some(category) = plate.category {
@@ -140,79 +139,79 @@ pub fn get_plate_category(s: &str, args: &Args) -> String {
                 NOT_FOUND.to_string()
             }
         }
-        Err(e) => ERROR.to_string(),
+        Err(_e) => ERROR.to_string(),
     }
 }
 // ---
-pub fn number_to_words(s: &str, args: &Args) -> String {
+pub fn number_to_words(s: &str) -> String {
     number_to_words::number_to_words_str(s).unwrap_or(ERROR.to_string())
 }
 // ---
-pub fn has_persian(s: &str, args: &Args) -> String {
+pub fn has_persian(s: &str) -> String {
     persian_chars::has_persian(s, false).to_string()
 }
-pub fn is_persian(s: &str, args: &Args) -> String {
+pub fn is_persian(s: &str) -> String {
     persian_chars::is_persian(s, false).to_string()
 }
-pub fn to_persian_chars(s: &str, args: &Args) -> String {
+pub fn to_persian_chars(s: &str) -> String {
     persian_chars::to_persian_chars(s)
 }
 // ---
-pub fn is_phone_valid(s: &str, args: &Args) -> String {
+pub fn is_phone_valid(s: &str) -> String {
     phone_number::is_phone_valid(s).is_ok().to_string()
 }
-pub fn get_operator_prefix(s: &str, args: &Args) -> String {
+pub fn get_operator_prefix(s: &str) -> String {
     phone_number::get_operator_prefix(s)
         .unwrap_or(INVALID)
         .to_string()
 }
-pub fn get_phone_operator(s: &str, args: &Args) -> String {
+pub fn get_phone_operator(s: &str) -> String {
     match phone_number::operators::get_phone_details(s) {
         Ok(x) => format!("{:?}", x.operator()),
         Err(_) => INVALID.to_string(),
     }
 }
-pub fn get_phone_province(s: &str, args: &Args) -> String {
+pub fn get_phone_province(s: &str) -> String {
     match phone_number::operators::get_phone_details(s) {
         Ok(x) => format!("{:?}", x.base()),
         Err(_) => INVALID.to_string(),
     }
 }
 // ---
-pub fn is_sheba_valid(s: &str, args: &Args) -> String {
+pub fn is_sheba_valid(s: &str) -> String {
     sheba::is_sheba_valid(s).is_ok().to_string()
 }
-pub fn sheba_to_bank_name(s: &str, args: &Args) -> String {
+pub fn sheba_to_bank_name(s: &str) -> String {
     match sheba::get_sheba_info(s) {
         Ok(x) => x.get_name().to_string(),
-        Err(e) => INVALID.to_string(),
+        Err(_e) => INVALID.to_string(),
     }
 }
-pub fn sheba_to_persian_bank_name(s: &str, args: &Args) -> String {
+pub fn sheba_to_persian_bank_name(s: &str) -> String {
     match sheba::get_sheba_info(s) {
         Ok(x) => x.get_persian_name().to_string(),
-        Err(e) => INVALID.to_string(),
+        Err(_e) => INVALID.to_string(),
     }
 }
 // ---
-pub fn time_diff(s: &str, args: &Args) -> String {
+pub fn time_diff(s: &str) -> String {
     match rust_persian_tools::time_diff::time_diff_now(s) {
-        Ok(timeDiff) => timeDiff.long_form(),
-        Err(e) => ERROR.to_string(),
+        Ok(time_diff) => time_diff.long_form(),
+        Err(_e) => ERROR.to_string(),
     }
 }
 // --
-pub fn url_fix(s: &str, args: &Args) -> String {
+pub fn url_fix(s: &str) -> String {
     url_fix::url_fix(s, None).unwrap_or(ERROR.to_string())
 }
 // --
-pub fn verify_card_number(s: &str, args: &Args) -> String {
+pub fn verify_card_number(s: &str) -> String {
     verity_card_number::verify_card_number(s)
         .is_ok()
         .to_string()
 }
 // ---
-pub fn number_to_words_str(s: &str, args: &Args) -> String {
+pub fn words_to_number(s: &str) -> String {
     words_to_number::words_to_number_str(
         s,
         &rust_persian_tools::words_to_number::Options::default(),
